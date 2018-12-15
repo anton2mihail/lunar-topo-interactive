04b81000838cd61c211ecac44b178e60762d3dd1
Anton Mihail Lachmaniucu <antonmihail@gmail.com>
2018-12-15 18:12:56 -0500

    Yeet

---
+++ Assets/Surface/SurfaceCreator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class SurfaceCreator : MonoBehaviour
+{
+
+  private Mesh mesh;
+
+  private void OnEnable()
+  {
+    if (mesh == null)
+    {
+      mesh = new Mesh();
+      mesh.name = "Surface Mesh";
+      GetComponent<MeshFilter>().mesh = mesh;
+    }
+    Refresh();
+  }
+
+  public void Refresh()
+  {
+    mesh.vertices = new Vector3[] {
+      new Vector3(0f, 0f),
+      new Vector3(1f, 0f),
+      new Vector3(0f, 1f)
+    };
+    mesh.triangles = new int[] {
+      0, 1, 2
+    };
+  }
+}
